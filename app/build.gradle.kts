168c27f056a78d935037f0296823ad1a1e7c9e23
timachado <contato.timachado@gmail.com>
2026-09-20 16:19:44 -0300

    release: bump para 0.21.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.timachado.fiolab"
         minSdk = 26
         targetSdk = 36
-        versionCode = 28
-        versionName = "0.21.7"
+        versionCode = 29
+        versionName = "0.21.8"
 
         buildConfigField(
             "String",
