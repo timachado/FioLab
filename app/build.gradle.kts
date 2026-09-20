796542216d5042380955a476db3a1509a21dc799
timachado <contato.timachado@gmail.com>
2026-09-20 15:04:19 -0300

    chore: preparar versao 0.21.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.timachado.fiolab"
         minSdk = 26
         targetSdk = 36
-        versionCode = 25
-        versionName = "0.21.4"
+        versionCode = 26
+        versionName = "0.21.5"
 
         buildConfigField(
             "String",
