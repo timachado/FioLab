bdc6adc0d17648b491fae1ceb28a96cb0af18273
timachado <contato.timachado@gmail.com>
2026-09-20 12:28:38 -0300

    fix: corrigir consume no desenho livre

--- app/src/main/java/com/timachado/fiolab/CreateDrawingScreen.kt
+++ app/src/main/java/com/timachado/fiolab/CreateDrawingScreen.kt
@@ -43,8 +43,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.input.pointer.consume
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
