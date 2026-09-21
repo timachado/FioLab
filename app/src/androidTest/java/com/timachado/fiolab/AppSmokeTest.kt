3105d0b1dfc8b7868550d9f6434fdb8010a94366
timachado <contato.timachado@gmail.com>
2026-09-21 05:26:47 -0300

    test: corrigir import do smoke test Compose

--- app/src/androidTest/java/com/timachado/fiolab/AppSmokeTest.kt
+++ app/src/androidTest/java/com/timachado/fiolab/AppSmokeTest.kt
@@ -1,6 +1,5 @@
 package com.timachado.fiolab
 
-import androidx.compose.ui.test.assertDoesNotExist
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
