ded2642e975a0d529b5208e7b51a4e2f4ac0bd0b
timachado <contato.timachado@gmail.com>
2026-09-21 04:28:33 -0300

    chore: bump FioLab to 0.44.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.timachado.fiolab"
         minSdk = 26
         targetSdk = 36
-        versionCode = 56
-        versionName = "0.43.0"
+        versionCode = 57
+        versionName = "0.44.0"
 
         buildConfigField(
             "String",
