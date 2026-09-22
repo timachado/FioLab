c3e90e6b1afd4a1802510c20d746e33c69485cce
timachado <contato.timachado@gmail.com>
2026-09-22 01:36:44 -0300

    chore: bump 0.48.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.timachado.fiolab"
         minSdk = 26
         targetSdk = 36
-        versionCode = 83
-        versionName = "0.48.2"
+        versionCode = 84
+        versionName = "0.48.3"
 
         buildConfigField(
             "String",
